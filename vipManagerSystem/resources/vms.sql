656def4069735652fe0bef8d67f33d413b0bc0be
wang_yjun <wang_yjun@wang_yjun-PC>
2017-07-29 07:05:36 +0800

    提交数据库文件

--- vipManagerSystem/resources/vms.sql
+++ vipManagerSystem/resources/vms.sql
@@ -1,22 +1,22 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 127.0.0.1
-Source Server Version : 50528
+Source Server         : localhost
+Source Server Version : 50130
 Source Host           : localhost:3306
 Source Database       : vms
 
 Target Server Type    : MYSQL
-Target Server Version : 50528
+Target Server Version : 50130
 File Encoding         : 65001
 
-Date: 2017-07-14 17:27:29
+Date: 2017-07-29 07:05:06
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for goods
+-- Table structure for `goods`
 -- ----------------------------
 DROP TABLE IF EXISTS `goods`;
 CREATE TABLE `goods` (
@@ -29,17 +29,37 @@ CREATE TABLE `goods` (
   `price` double(10,2) DEFAULT NULL,
   `remark` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
-INSERT INTO `goods` VALUES ('1', '11', '红茶', '饮品', '11', '1', '11.00', '11');
-INSERT INTO `goods` VALUES ('3', '2', '布丁', '点心', '222', '2', '222.00', '2');
-INSERT INTO `goods` VALUES ('4', '3', '绿茶', '饮品', '33', '33', '33.00', '33');
+INSERT INTO `goods` VALUES ('8', 'K03', '卡布奇诺', '咖啡', '350ml', '杯', '25.00', '');
+INSERT INTO `goods` VALUES ('7', 'K02', '拿铁咖啡', '咖啡', '350ml', '杯', '25.00', '');
+INSERT INTO `goods` VALUES ('6', 'K01', '美式咖啡', '咖啡', '350ml', '杯', '18.00', '');
+INSERT INTO `goods` VALUES ('9', 'K04', '邦咖啡', '咖啡', '350ml', '杯', '35.00', '含酒精');
+INSERT INTO `goods` VALUES ('11', 'L01', '蓝柑蜜柚', '冷饮', '500ml', '杯', '20.00', '');
+INSERT INTO `goods` VALUES ('12', 'L02', '玫瑰青提', '冷饮', '350ml', '杯', '20.00', '');
+INSERT INTO `goods` VALUES ('13', 'L03', '冻柠茶', '冷饮', '350ml', '杯', '20.00', '');
+INSERT INTO `goods` VALUES ('14', 'L04', '手工奶茶', '冷饮', '500ml', '杯', '20.00', '');
+INSERT INTO `goods` VALUES ('15', 'L05', '柠檬汁', '冷饮', '500ml', '杯', '18.00', '');
+INSERT INTO `goods` VALUES ('16', 'L06', '芒果冰沙', '冷饮', '350ml', '杯', '20.00', '');
+INSERT INTO `goods` VALUES ('17', 'C02', '英式红茶', '茶', '1L', '壶', '48.00', '');
+INSERT INTO `goods` VALUES ('18', 'C01', '英式红茶（杯）', '茶', '350ml', '杯', '18.00', '');
+INSERT INTO `goods` VALUES ('19', 'C03', '陈年普洱（杯）', '茶', '350ml', '杯', '25.00', '');
+INSERT INTO `goods` VALUES ('20', 'C04', '陈年普洱', '茶', '1L', '壶', '68.00', '');
+INSERT INTO `goods` VALUES ('21', 'C05', '金边玫瑰（杯）', '茶', '350ml', '杯', '25.00', '');
+INSERT INTO `goods` VALUES ('22', 'C06', '金边玫瑰', '茶', '1L', '壶', '68.00', '');
+INSERT INTO `goods` VALUES ('23', 'C07', '糯米香普（杯）', '茶', '350ml', '杯', '25.00', '');
+INSERT INTO `goods` VALUES ('24', 'C08', '糯米香普', '茶', '1L', '壶', '68.00', '');
+INSERT INTO `goods` VALUES ('25', 'S01', '泡鲁达', '特色', '350ml', '杯', '25.00', '');
+INSERT INTO `goods` VALUES ('26', 'J01', '莫吉托', '特色', '350ml', '杯', '30.00', '');
+INSERT INTO `goods` VALUES ('27', 'J02', '秀兰邓波', '特色', '350ml', '杯', '30.00', '');
+INSERT INTO `goods` VALUES ('28', 'J03', '金汤力', '特色', '350ml', '杯', '30.00', '');
+INSERT INTO `goods` VALUES ('29', 'J04', '长岛冰茶', '特色', '350ml', '杯', '35.00', '');
 
 -- ----------------------------
--- Table structure for t_order
+-- Table structure for `t_order`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_order`;
 CREATE TABLE `t_order` (
@@ -53,24 +73,15 @@ CREATE TABLE `t_order` (
   `count` int(11) DEFAULT NULL,
   `discount` double(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_order
 -- ----------------------------
-INSERT INTO `t_order` VALUES ('8', '2', '112', '1', null, '233.00', null, '2', null);
-INSERT INTO `t_order` VALUES ('9', '4', 'aacasd', '1', null, '110.00', null, '10', null);
-INSERT INTO `t_order` VALUES ('10', '3', 'bb', '11', null, '233.00', null, '2', null);
-INSERT INTO `t_order` VALUES ('11', '2', '112', '11', null, '454.00', null, '3', '1.00');
-INSERT INTO `t_order` VALUES ('12', '3', 'aaaaaa', '11', null, '111.00', null, '1111', null);
-INSERT INTO `t_order` VALUES ('13', null, 'm,nlkmjkl', '111', null, '11.00', null, '1', null);
-INSERT INTO `t_order` VALUES ('14', null, 'hhh', '111', null, '11.00', null, '1', null);
-INSERT INTO `t_order` VALUES ('15', '3', 'aaaaaa', 'iuiui', null, '55.00', '01', '5', null);
-INSERT INTO `t_order` VALUES ('16', '2', '112', '1111', '2017-07-12', '21.00', null, '2', '1.00');
-INSERT INTO `t_order` VALUES ('17', '2', '112', '', '2017-07-14', '232.00', '01', '2', '1.00');
+INSERT INTO `t_order` VALUES ('3', null, '淡然', '201707280950', '2017-07-28', '45.00', '01', '2', null);
 
 -- ----------------------------
--- Table structure for t_order_detail
+-- Table structure for `t_order_detail`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_order_detail`;
 CREATE TABLE `t_order_detail` (
@@ -79,27 +90,16 @@ CREATE TABLE `t_order_detail` (
   `goodId` int(11) DEFAULT NULL,
   `count` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_order_detail
 -- ----------------------------
-INSERT INTO `t_order_detail` VALUES ('3', '8', '1', '1');
-INSERT INTO `t_order_detail` VALUES ('4', '8', '3', '1');
-INSERT INTO `t_order_detail` VALUES ('17', '9', '1', '10');
-INSERT INTO `t_order_detail` VALUES ('19', '13', '1', '1');
-INSERT INTO `t_order_detail` VALUES ('20', '14', '1', '1');
-INSERT INTO `t_order_detail` VALUES ('22', '10', '1', '1');
-INSERT INTO `t_order_detail` VALUES ('23', '10', '3', '1');
-INSERT INTO `t_order_detail` VALUES ('26', '11', '1', '1');
-INSERT INTO `t_order_detail` VALUES ('27', '11', '3', '2');
-INSERT INTO `t_order_detail` VALUES ('28', '16', '1', '2');
-INSERT INTO `t_order_detail` VALUES ('29', '17', '1', '1');
-INSERT INTO `t_order_detail` VALUES ('30', '17', '3', '1');
-INSERT INTO `t_order_detail` VALUES ('31', '15', '1', '5');
+INSERT INTO `t_order_detail` VALUES ('5', '3', '8', '1');
+INSERT INTO `t_order_detail` VALUES ('6', '3', '11', '1');
 
 -- ----------------------------
--- Table structure for t_recharge
+-- Table structure for `t_recharge`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_recharge`;
 CREATE TABLE `t_recharge` (
@@ -113,42 +113,36 @@ CREATE TABLE `t_recharge` (
   `totalPoints` int(11) DEFAULT NULL,
   `rechargetime` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_recharge
 -- ----------------------------
-INSERT INTO `t_recharge` VALUES ('2', '2', null, '1', '2', '16', '1', '115', null);
-INSERT INTO `t_recharge` VALUES ('3', '4', null, '1111', '111', '1', '111', '1', null);
-INSERT INTO `t_recharge` VALUES ('4', '3', null, '11', '111', null, '111', '112', null);
-INSERT INTO `t_recharge` VALUES ('5', '3', null, '111', '111', null, '11', '134', null);
-INSERT INTO `t_recharge` VALUES ('6', '2', null, '1', '1', '1', '1', '1', null);
-INSERT INTO `t_recharge` VALUES ('7', '2', null, '10', '1', '12', '2', '113', null);
-INSERT INTO `t_recharge` VALUES ('8', '2', null, '1', '1', '1', '1', '1', null);
-INSERT INTO `t_recharge` VALUES ('9', '3', null, '1', '1', '16', '1', '115', '2017-07-12');
-INSERT INTO `t_recharge` VALUES ('10', '2', null, '1', '1', '16', '1', '115', '2017-07-12');
+INSERT INTO `t_recharge` VALUES ('5', '13', null, '1500', '600', '2100', '15', '15', '2017-07-28');
+INSERT INTO `t_recharge` VALUES ('6', '14', null, '1500', '500', '2000', '1500', '1500', '2017-07-29');
+INSERT INTO `t_recharge` VALUES ('7', '15', null, '1500', '500', '2000', '100', '100', '2017-07-29');
 
 -- ----------------------------
--- Table structure for t_user
+-- Table structure for `t_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `VIP` varchar(20) DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL,
-  `sex` varchar(1) DEFAULT NULL,
+  `sex` varchar(1) DEFAULT '1',
   `birthday` varchar(20) DEFAULT NULL,
   `address` varchar(200) DEFAULT NULL,
-  `telephone` varchar(10) DEFAULT NULL,
-  `balance` double(8,3) DEFAULT NULL COMMENT '余额',
-  `score` int(11) DEFAULT NULL,
-  `level` int(11) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `balance` double(8,3) DEFAULT '0.000' COMMENT '余额',
+  `score` int(11) DEFAULT '0',
+  `level` varchar(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('2', '123', '112', '1', '2017-06-24', '1', '11', '1.000', '1', '111');
-INSERT INTO `t_user` VALUES ('3', 'aaaa', 'aaaaaa', '0', '2017-06-16', 'aaa', 'aaa', '123.000', '123', '1');
-INSERT INTO `t_user` VALUES ('4', '1', 'aacasd', null, '', '1', '1', '1.000', '1', '1');
+INSERT INTO `t_user` VALUES ('15', null, '麦克', '1', null, null, null, '2000.000', '100', null);
+INSERT INTO `t_user` VALUES ('14', null, '麦克', '1', null, null, null, '2000.000', '1500', null);
+INSERT INTO `t_user` VALUES ('13', '0001', '淡然', '0', '', '', '', null, null, '');
